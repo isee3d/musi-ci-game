36cc5be72e8873eaf064f2a2f57682e32abfb9f1
Thimo <thimoderam@gmail.com>
2023-03-26 14:00:13 +0200

    [Added] nextpage snippet and react component snippet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
-import Image from 'next/image'
+import Image from 'next/image';
 
 const Welcome: NextPage = () => {
   // const { data: session } = useSession();
