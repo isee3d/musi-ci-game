6914592e6f326d80c946317ec8628ab0c76b6922
Thimo <thimoderam@gmail.com>
2023-04-14 14:28:18 +0200

    [Added] react three fiber to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,23 +4,36 @@ import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 
 import { api } from "~/utils/api";
-
 import Layout from '../components/layout';
 import Head from "next/head";
+import Header from '~/config'
 
-import "~/styles/globals.css";
 import { ToneJSService } from "~/components/fragmentPlayer/audioService/ToneJSService";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import AudioService from "~/components/fragmentPlayer/audioService/AudioService";
+import dynamic from "next/dynamic";
+import { NextComponentType, NextPageContext } from "next";
+import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const Scene = dynamic(() => import("~/components/canvas/Scene"), { ssr: true });
+
+type CustomComponentType = NextComponentType<NextPageContext, any, any> & {
+  canvas?: (pageProps: any) => JSX.Element;
+};
+
+const MyApp: AppType<{ session: Session | null, title?: string }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { session, title = "index", ...pageProps },
 }) => {
+
+  const ref = useRef(null);
+
   useEffect(() => {
     ToneJSService.init();
     AudioService.init();
   }, []);
+
+  const CustomComponent = Component as CustomComponentType;
 
   return (
     <SessionProvider session={ session }>
@@ -30,8 +43,19 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
+        <Header title={ title } />
         <Toaster position="bottom-center" />
-        <Component { ...pageProps } />
+        <div
+          ref={ ref }
+          className="absolute left-0 top-0 z-10 h-screen w-screen overflow-hidden bg-zinc-900 text-gray-50">
+          <Component { ...pageProps } />
+          { CustomComponent?.canvas && (
+            <Scene className="pointer-events-none" eventSource={ ref } eventPrefix="client">
+              { CustomComponent.canvas(pageProps) }
+            </Scene>
+          ) }
+        </div>
+        {/* <Component { ...pageProps } /> */ }
       </Layout>
     </SessionProvider>
   );
