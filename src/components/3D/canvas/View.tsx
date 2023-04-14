b6569124a7fc3a1ed22f46dc520c4740ed993c4b
Thimo <thimoderam@gmail.com>
2023-04-14 19:34:12 +0200

    [Added] type for color prop

--- src/components/3D/canvas/View.tsx
+++ src/components/3D/canvas/View.tsx
@@ -3,8 +3,13 @@
 import React, { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
 import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
 import { Three } from '~/components/3D/helpers/components/Three'
+import { ColorRepresentation } from 'three';
 
-export const Common = ({ color }: any) => (
+interface CommonProps {
+  color?: ColorRepresentation;
+}
+
+export const Common = ({ color }: CommonProps) => (
   <Suspense fallback={ null }>
     { color && <color attach='background' args={ [color] } /> }
     <ambientLight intensity={ 0.5 } />
