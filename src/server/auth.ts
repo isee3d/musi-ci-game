647d3502f73ad9aeb78509e81e9c0e87d310287a
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-05-02 16:11:12 +0200

    [WIP] adding credentials provider and checking user in DB

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,6 +7,7 @@ import {
 } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -57,6 +58,36 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      type: "credentials",
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "me@gmail.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+          const {email, password} = credentials as {email: string, password: string};
+
+          const user = await prisma.user.findUnique({
+            where: { email: email },
+          })
+
+          if(!user){
+            // TODO: throw error
+            return null;
+          }
+
+          // hash given password and compare hashes
+          const isValid = user.id === password;
+
+
+
+          // Check if user exists in DB
+          // Get the user and his role from DB
+
+          return {id: "1", name: "John Doe", email: "john@gmail.com", role: "USER"};
+      }
     })
     /**
      * ...add more providers here.
@@ -68,6 +99,9 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  // pages: {
+  //   signIn: "/auth/signin",
+  // }
 };
 
 /**
