8dfa7a9cee7c633194137b2a45ea12f05808ff47
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-05-22 09:25:41 +0200

    [Fixed] packages updated and fixed env files

--- src/env.mjs
+++ src/env.mjs
@@ -44,7 +44,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
       DATABASE_URL: process.env.DATABASE_URL,
-      NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
+      NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
       NEXT_PUBLIC_XSTATE_DEV_TOOLS: process.env.NEXT_PUBLIC_XSTATE_DEV_TOOLS,
       NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
       NEXTAUTH_URL: process.env.NEXTAUTH_URL,
