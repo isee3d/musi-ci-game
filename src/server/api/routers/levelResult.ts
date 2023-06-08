a217496adc58ec942a900fa5f678c1c40e1b11c2
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-06-08 11:48:40 +0200

    [Added] prisma create structure

--- src/server/api/routers/levelResult.ts
+++ src/server/api/routers/levelResult.ts
@@ -11,8 +11,36 @@ export const levelResultRouter = createTRPCRouter({
         .input(LevelResultOptionalDefaultsWithRelationsSchema)
         .mutation(async ({ ctx, input }) => {
             return await ctx.prisma.levelResult.create({
-                data: input,
-                // TODO: split all data from input to create correct create object
+                data: {
+                    id_User: input.id_User,
+                    id_level: input.id_level,
+                    id_subLevel: input.id_subLevel,
+                    id_gameMode: input.id_gameMode,
+                    answeredCorrectlyAmount: input.answeredCorrectlyAmount,
+                    answeredIncorrectlyAmount: input.answeredIncorrectlyAmount,
+                    startTime: input.startTime,
+                    endTime: input.endTime,
+                    score: input.score,
+                    Scenes: {
+                        create: input.Scenes.map((scene) => ({
+                            chosenFragmentLatency: scene.chosenFragmentLatency ?? 0,
+                            sceneFragments: {
+                                create: scene.sceneFragments.map((fragment) => ({
+                                    fragmentIndex: fragment.fragmentIndex,
+                                    isCorrectFragment: fragment.isCorrectFragment,
+                                    isPlayedFragment: fragment.isPlayedFragment,
+                                    groundTone: fragment.groundTone,
+                                })),
+                            },
+                            relistenFragments: {
+                                create: scene.relistenFragments.map((relistenFragment) => ({
+                                    id_fragment: relistenFragment.id_fragment,
+                                    relistenCount: relistenFragment.relistenCount,
+                                })),
+                            },
+                        })),
+                    }
+                },
             });
         }),
 
