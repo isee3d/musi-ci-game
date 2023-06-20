30400f538698f24050442acb993467c99ae81a5c
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-06-20 11:44:02 +0200

    [fixed] modepage styling

--- src/pages/[levelId]/[subLevel]/[mode].tsx
+++ src/pages/[levelId]/[subLevel]/[mode].tsx
@@ -9,8 +9,9 @@ import Spelen from '~/components/gameModes/spelen/spelen'
 import { spelenMachine } from '~/components/gameModes/spelen/spelenMachine'
 import Uitdaging from '~/components/gameModes/uitdaging/uitdaging'
 import { uitdagingMachine } from '~/components/gameModes/uitdaging/uitdagingMachine'
-import Sublevel from '~/components/subLevel'
+import { Button } from '~/components/ui/button'
 import { env } from '~/env.mjs'
+import { cn } from '~/lib/utils'
 import { generateServerSideHelper } from '~/server/helpers/serverSideHelper'
 import { useAudioServiceStore } from '~/stores/useAudioServiceStore'
 import { api } from '~/utils/api'
@@ -19,12 +20,15 @@ export const SpelenMachineContext = createActorContext(spelenMachine, { devTools
 export const UitdagingMachineContext = createActorContext(uitdagingMachine, { devTools: true })
 export const LuisterenMachineContext = createActorContext(luisterenMachine, { devTools: true })
 
-const Mode: NextPage<{ levelId: string; subLevel: string; mode: string }> = ({
+const tabs = ['Luisteren', 'Spelen', 'Uitdaging']
+
+const ModePage: NextPage<{ levelId: string; subLevel: string; mode: string }> = ({
   levelId,
   subLevel,
   mode,
 }) => {
   const fragmentLevelQuery = api.sublevel.getFragmentsOfSublevel.useQuery({ subLevelId: subLevel })
+  const sublevelQuery = api.sublevel.getSublevelById.useQuery({ id: subLevel })
   const modeQuery = api.gameMode.getGameMode.useQuery({ name: mode })
   const router = useRouter()
   const { audioContext } = useAudioServiceStore()
@@ -32,11 +36,11 @@ const Mode: NextPage<{ levelId: string; subLevel: string; mode: string }> = ({
   const fragments = fragmentLevelQuery?.data?.fragments ?? []
   const playTime = fragmentLevelQuery?.data?.playTime
 
-  useEffect(() => {
-    if (!audioContext && env.NEXT_PUBLIC_ENABLE_AUDIO) {
-      router.push(`/modeSelect/${levelId}/${subLevel}`)
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (!audioContext && env.NEXT_PUBLIC_ENABLE_AUDIO) {
+  //     router.push(`/modeSelect/${levelId}/${subLevel}`)
+  //   }
+  // }, [])
 
   function renderGameMode(mode: string) {
     switch (mode) {
@@ -92,31 +96,33 @@ const Mode: NextPage<{ levelId: string; subLevel: string; mode: string }> = ({
         <meta name="description" content="Level name here" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex grow flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <h1 className="mb-10 py-3 text-center text-8xl font-extrabold tracking-tight text-white ">
-          {subLevel}
-        </h1>
-        <div className="container mx-auto flex flex-col items-center justify-center rounded-2xl border-4 border-white ">
-          {/* Title */}
-          <div className="mb-4 flex w-full justify-around ">
-            <h2 className="grow rounded-l-xl border-4 border-white py-3 text-center text-3xl font-extrabold tracking-tight text-white ">
-              {mode}
-            </h2>
-            <h2 className="grow border-4 border-white bg-gray-400 py-3 text-center text-3xl font-extrabold tracking-tight text-white ">
-              Spelen
-            </h2>
-            <h2 className="grow rounded-r-xl border-4 border-white bg-gray-400 py-3 text-center text-3xl font-extrabold text-white">
-              Uitdaging
-            </h2>
+
+      <section className="flex grow flex-col items-center justify-center">
+        <h2 className="mb-10 py-3 text-center text-8xl font-extrabold tracking-tight">
+          {sublevelQuery?.data?.name}
+        </h2>
+        <div className="container mx-auto flex flex-col items-center justify-center rounded-2xl border-4 border-primary p-0">
+          <div className="mb-4 flex w-full justify-around">
+            {tabs.map((tab, index) => (
+              <Button
+                key={index}
+                size={'lg'}
+                className={cn(
+                  'h-16 grow border-4 border-primary bg-gray-400 py-3 text-center text-3xl font-extrabold tracking-tight',
+                  mode === tab ? 'text-red-500' : 'text-primary',
+                  index === 0 && 'rounded-l-xl',
+                  index === tabs.length - 1 && 'rounded-r-xl'
+                )}
+              >
+                {tab}
+              </Button>
+            ))}
           </div>
-          {/* <h3 className="pt-4 text-center text-2xl font-extrabold tracking-tight text-white">
-                    Kijk en luister
-                </h3> */}
           <div className="flex w-1/2 flex-col justify-center space-y-8 p-5">
             {renderGameMode(mode)}
           </div>
         </div>
-      </main>
+      </section>
     </>
   )
 }
@@ -132,7 +138,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   await ssg.sublevel.getFragmentsOfSublevel.prefetch({ subLevelId: subLevel })
   await ssg.gameMode.getGameMode.prefetch({ name: mode })
-  // await ssg.   Do the prefetch of the level and data here
 
   return {
     props: {
@@ -148,4 +153,4 @@ export const getStaticPaths = () => {
   return { paths: [], fallback: 'blocking' }
 }
 
-export default Mode
+export default ModePage
