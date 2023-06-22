d2b94d327fc54dfea910148cbe2c9f2f189e14a9
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-06-22 13:37:01 +0200

    [Added] base page for sublevels

--- src/components/manage/updateFragmentModal.tsx
+++ src/components/manage/updateFragmentModal.tsx
@@ -1,9 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Fragment, Note } from '@prisma/client'
-import Head from 'next/head'
+import { Fragment } from '@prisma/client'
 import { FragmentOptionalDefaultsSchema } from 'prisma/generated/zod'
 import { useState } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import toast from 'react-hot-toast'
 import { NoteCreate } from 'types/Note'
 import { z } from 'zod'
@@ -27,11 +26,6 @@ interface BaseStaticModalProps {
   setmodal: React.Dispatch<React.SetStateAction<boolean>>
   fragment: Fragment
 }
-
-// const validationRules = {
-//   name: { required: 'Note is required.' },
-//   description: { required: 'Description is required.' },
-// }
 
 const UpdateFragmentModal: React.FC<BaseStaticModalProps> = ({ setmodal, fragment }) => {
   const [newNotes, setNewNotes] = useState<NoteCreate[]>([])
@@ -130,110 +124,6 @@ const UpdateFragmentModal: React.FC<BaseStaticModalProps> = ({ setmodal, fragmen
       </div>
     </>
   )
-  //   const [newNotes, setNewNotes] = useState<Note[]>([])
-  //   const {
-  //     register,
-  //     handleSubmit,
-  //     reset,
-  //     formState: { errors, isValid },
-  //   } = useForm<Fragment>({ mode: 'onBlur' })
-
-  //   const { mutate: updateFragment } = api.fragmentNote.updateFragment.useMutation()
-  //   const notesOfFragmentQuery = api.fragmentNote.getNotesOfFragment.useQuery(
-  //     { id: fragment.id },
-  //     { onSuccess: (data) => setNewNotes(data) }
-  //   )
-
-  //   const onSubmit: SubmitHandler<Fragment> = (data) => {
-  //     updateFragment({ ...data, notes: newNotes, id: fragment.id })
-  //     setNewNotes([])
-  //     reset()
-  //     setmodal(false)
-  //   }
-
-  //   return (
-  //     <>
-  //       <Head>
-  //         <title>Manage fragments</title>
-  //         <meta name="description" content="Level name here" />
-  //         <link rel="icon" href="/favicon.ico" />
-  //       </Head>
-
-  //       <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-  //         <div className="relative mx-auto my-6 w-auto max-w-3xl">
-  //           {/*content*/}
-  //           <div className="relative flex w-full flex-col rounded-lg border-0 bg-gray-400 shadow-lg outline-none focus:outline-none">
-  //             {/*header*/}
-  //             <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
-  //               <h3 className="text-3xl font-semibold text-black">Update Fragment</h3>
-  //             </div>
-  //             <div className="relative flex flex-col justify-center p-6">
-  //               <form onSubmit={handleSubmit(onSubmit)}>
-  //                 <div className="grid w-full gap-6 md:grid-cols-2">
-  //                   <div>
-  //                     <label className="mb-2 block text-sm font-medium text-gray-900 ">Name</label>
-  //                     <input
-  //                       {...register('name', validationRules.name)}
-  //                       type="text"
-  //                       autoComplete="off"
-  //                       id="first_name"
-  //                       data-lpignore="true"
-  //                       className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700  dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-  //                       placeholder="bijv: gelijk: twee gelijke noten"
-  //                       value={fragment.name}
-  //                       required
-  //                     />
-  //                     <p className="text-red-600">{errors.name?.message}</p>
-  //                   </div>
-  //                   <div>
-  //                     <label className="mb-2 block text-sm font-medium text-gray-900 ">
-  //                       Luister fragment
-  //                     </label>
-  //                     <button className="rounded-xl bg-blue-500 p-4  hover:bg-white/20 ">
-  //                       Luister naar fragment
-  //                     </button>
-  //                   </div>
-  //                   <div>
-  //                     <label className="mb-2 block text-sm font-medium text-gray-900 ">
-  //                       Omschrijving
-  //                     </label>
-  //                     <textarea
-  //                       {...register('description', validationRules.description)}
-  //                       placeholder="Vul hier een omschrijving in"
-  //                       autoComplete="off"
-  //                       value={fragment.description ?? ''}
-  //                       className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700  dark:placeholder:text-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-  //                       required
-  //                     />
-  //                     <p className="text-red-600">{errors.description?.message}</p>
-  //                   </div>
-  //                 </div>
-  //                 {newNotes?.map((note) => (
-  //                   <ExistingNote key={note.id + Math.random() * 58} {...note} />
-  //                 ))}
-  //                 <button
-  //                   type="submit"
-  //                   disabled={!isValid && newNotes.length > 0}
-  //                   className={`m-2 min-w-[50vh] rounded-xl text-white  ${
-  //                     isValid && newNotes.length > 0
-  //                       ? 'bg-green-500 hover:bg-green-600'
-  //                       : 'cursor-not-allowed bg-gray-400'
-  //                   }`}
-  //                 >
-  //                   <h3 className="text-center text-2xl font-bold">Fragment wijzigingen opslaan</h3>
-  //                 </button>
-  //               </form>
-  //               <div className="mt-4 flex flex-col space-y-3 border-4 pt-1 shadow-xl shadow-fuchsia-500">
-  //                 <h3 className="text-2xl text-black">Noten toevoegen of verwijderen</h3>
-  //                 <NoteCreator setNewNotes={setNewNotes} />
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-  //     </>
-  //   )
 }
 
 export default UpdateFragmentModal
--- src/pages/manage/sublevels.tsx
+++ src/pages/manage/sublevels.tsx
@@ -3,6 +3,10 @@ import { type NextPage } from 'next';
 import { SubLevel } from '@prisma/client';
 import { useForm } from 'react-hook-form';
 import { api } from '~/utils/api';
+import { Button, buttonVariants } from '~/components/ui/button';
+import { useState } from 'react';
+import ManageBaseModal from '~/components/manage/manageBaseModal';
+import { cn } from '~/lib/utils';
 
 const ManageSublevels: NextPage = () => {
     const { register, handleSubmit, reset, formState: { errors, isValid } } = useForm<SubLevel>({ mode: 'onBlur' });
@@ -10,18 +14,68 @@ const ManageSublevels: NextPage = () => {
     const { mutate: deleteSubLevel } = api.sublevel.deleteSubLevel.useMutation();
     const subLevelQuery = api.sublevel.getAllSubLevels.useQuery();
 
+    const [createModal, setCreateModal] = useState(false)
+    const [showModal, setShowModal] = useState(false)
+     const [selectedSubLevel, setSelectedSubLevel] = useState<SubLevel | null>(null)
+
   return (
     <>
       <Head>
         <title></title>
-        <meta name='description' content='' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
 
-      </main>
+      <section className="flex grow flex-col items-center justify-center">
+        <h1 className="mb-10 py-3 text-center text-4xl font-extrabold tracking-tight ">
+          Sublevel maken
+        </h1>
+        <div className="container mx-auto flex flex-col items-center justify-center rounded-2xl border-4">
+          <Button onClick={() => setCreateModal(true)} variant="outline">
+            Maak Een nieuw sublevel
+          </Button>
+          {createModal && (
+            <ManageBaseModal title="Nieuw Sublevel maken">
+              <CreateFragmentModal setmodal={setCreateModal} />
+            </ManageBaseModal>
+          )}
+          <div className="flex w-full flex-col gap-y-4 py-4">
+            {subLevelQuery.data?.map((sublevel) => {
+              return (
+                <div
+                  key={sublevel.id}
+                  className="grid min-w-full grid-cols-[1fr,auto,auto,auto,auto] items-center gap-4 rounded-md border-2 border-primary bg-primary/40 p-4"
+                >
+                  <h2 className="text-2xl font-bold">{sublevel.name}</h2>
+                  <h2 className="text-xl">{sublevel.description}</h2>
+                  <Button
+                    onClick={() => deleteSubLevel({ id: sublevel.id })}
+                    className={cn(buttonVariants({ variant: 'destructive', size: 'lg' }), 'px-4')}
+                  >
+                    verwijderen
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setSelectedSubLevel(sublevel)
+                      setShowModal(true)
+                    }}
+                    className={cn(buttonVariants({ variant: 'ghost', size: 'lg' }), 'px-4')}
+                  >
+                    Aanpassen
+                  </Button>
+                  {showModal && selectedSubLevel?.id === sublevel.id && (
+                    <ManageBaseModal title="Fragment updaten">
+                      <UpdateFragmentModal setmodal={setShowModal} fragment={sublevel} />
+                    </ManageBaseModal>
+                  )}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
     </>
-  );
+  )
 };
 
 export default ManageSublevels;
