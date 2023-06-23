4fc4367b9bc59d4f094528166efe328c0eeaacff
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-06-23 13:44:22 +0200

    [Fixed] login navbar button

--- src/components/3D/dom/Layout.tsx
+++ src/components/3D/dom/Layout.tsx
@@ -5,11 +5,11 @@ import dynamic from 'next/dynamic'
 import Footer from '~/components/footer'
 import Link from 'next/link'
 import { cn } from '~/lib/utils'
-import { buttonVariants } from '~/components/ui/button'
+import { Button, buttonVariants } from '~/components/ui/button'
 import { MainNav } from '~/components/mainNav'
 import { NavItem } from '~/components/mobileNav'
 import { ModeToggle } from '~/components/modeToggle'
-import { useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 const Scene = dynamic(() => import('~/components/3D/canvas/Scene'), { ssr: false })
 
 type LayoutProps = {
@@ -43,17 +43,15 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [mainNavItems, setMainNavItems] = useState<NavItem[]>(navitemsTemplate)
   const { data: sessionData } = useSession()
 
-useEffect(() => {
-  if (sessionData?.user?.role === 'ADMIN') {
-    setMainNavItems(navitemsTemplate)
-  } else {
-    setMainNavItems([])
-  }
-}, [sessionData])
-
+  useEffect(() => {
+    if (sessionData?.user?.role === 'ADMIN') {
+      setMainNavItems(navitemsTemplate)
+    } else {
+      setMainNavItems([])
+    }
+  }, [sessionData])
 
   const ref = useRef(null)
-
 
   return (
     <div ref={ref} className="relative h-full w-full overflow-auto" style={{ touchAction: 'auto' }}>
@@ -62,9 +60,12 @@ useEffect(() => {
           <div className="flex h-20 items-center justify-between py-6">
             <MainNav items={mainNavItems} />
             <nav className="flex gap-2">
-              <Link href="/login" className={cn(buttonVariants({ variant: 'secondary' }), 'px-4')}>
-                Login
-              </Link>
+              <Button
+                onClick={sessionData ? () => void signOut() : () => void signIn()}
+                className={cn(buttonVariants({ variant: 'secondary' }), 'px-4')}
+              >
+                {sessionData ? 'Uitloggen' : 'Inloggen'}
+              </Button>
               <ModeToggle />
             </nav>
           </div>
