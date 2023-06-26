d68327a0725c3fc455baa453578254cdbfa72ad4
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-06-26 16:14:46 +0200

    [Fixed] footer styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,18 @@
-import Link from "next/link";
+import Link from 'next/link'
+import { useCallback } from 'react'
 
 const footer: React.FC = () => {
-    return (<div className="relative bottom-0 w-full bg-gray-200 py-2 px-4 text-xs text-gray-600">
-        <i className="relative left-0">v1.0.5</i>
-        <Link href="https://www.musi-ci.nl/">Musi-CI Game ©2023 JokeVeltmanMuziek</Link>
-    </div>);
+  const getCurrentYear = useCallback(() => {
+    const currentDate = new Date()
+    return currentDate.getFullYear()
+  }, [])
+
+  return (
+    <div className="relative bottom-0 flex w-full justify-end gap-2 border-4 bg-background px-4 py-2 text-xs">
+      <h3>V2.0.0</h3>
+      <Link href="https://www.musi-ci.nl/">Musi-CI Game ©{getCurrentYear()} JokeVeltmanMuziek</Link>
+    </div>
+  )
 }
 
-export default footer;
+export default footer
