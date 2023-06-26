d1623c4317736c316c357fda53d5a086311a859e
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-06-26 15:36:39 +0200

    [Hotfix] removed participant id from check if user just logged in

--- src/hooks/useRequireAuth.ts
+++ src/hooks/useRequireAuth.ts
@@ -4,17 +4,13 @@ import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
 export const useRequireAuth = () => {
-  const { status, data: sessionData, update } = useSession()
+  const { status, data: sessionData } = useSession()
   const router = useRouter()
-
-  useEffect(() => {
-    update()
-  }, [])
 
   useEffect(() => {
     if (status === 'loading') return // Wait for loading to complete
     if (status === 'unauthenticated') router.push('/login')
-    if (!sessionData?.user?.participantId) router.push('/login')
+    // if (!sessionData?.user?.participantId) router.push('/login')
   }, [status, router])
 
   return sessionData
