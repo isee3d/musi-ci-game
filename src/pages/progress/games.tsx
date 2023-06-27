fec61dc603e6adce1762663933932e3e1b4b5d7b
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-06-27 15:52:03 +0200

    [fixed] styling game page

--- src/pages/progress/games.tsx
+++ src/pages/progress/games.tsx
@@ -19,22 +19,22 @@ const UserGamesPage: NextPage = () => {
       {gamesOfUserQuery.data?.map((game) => (
         <Link
           key={game.id}
-          className={cn(buttonVariants({ size: 'lg' }), 'h-20 rounded-xl')}
+          className={cn(buttonVariants({ size: 'lg' }), 'h-20 w-full rounded-xl')}
           href={`/progress/${game.id}`}
         >
-          <div className="flex w-full items-center justify-between">
-            <div className="flex justify-start space-x-4">
+          <div className="flex w-full items-center justify-center">
+            <div className="flex w-full justify-start space-x-4">
               <div
-                className=" flex h-16 w-5/6 items-center justify-center rounded-lg border-4 text-center text-2xl font-bold"
+                className="flex h-16 w-1/3 items-center justify-center rounded-lg border-4 text-center text-2xl font-bold"
                 // style={{ borderColor: 'green' }}
               >
                 {game.id}
               </div>
-              <div className=" flex h-16 w-48 items-center justify-center text-2xl font-medium">
+              <div className=" flex h-16 w-2/6 items-center justify-center text-2xl font-medium">
                 {game.name}
               </div>
             </div>
-            <div className=" flex h-16 w-16 items-center justify-center text-2xl font-medium">
+            <div className=" flex h-16 w-4/6 items-center justify-center text-2xl font-medium">
               {game.description}
             </div>
           </div>
