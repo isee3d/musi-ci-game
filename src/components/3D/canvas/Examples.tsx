895bb60ae8819b1981bae7c8418322308965448c
Thimo <thimoderam@gmail.com>
2023-07-06 19:49:06 +0200

    [Added] circles adt lines ends

--- src/components/3D/canvas/Examples.tsx
+++ src/components/3D/canvas/Examples.tsx
@@ -1,11 +1,9 @@
 // 'use client'
 
-import { Circle, Plane, useGLTF } from '@react-three/drei'
+import { Circle } from '@react-three/drei'
 import { useFrame, extend, Object3DNode, MaterialNode } from '@react-three/fiber'
 import * as THREE from 'three'
-import { Ref, useMemo, useRef, useState, useEffect } from 'react'
-import { Line, useCursor, MeshDistortMaterial } from '@react-three/drei'
-import { useRouter } from 'next/navigation'
+import { useRef } from 'react'
 import { NotePositionTime } from '~/components/fragmentPlayer/animationPlayer'
 import { MeshLineGeometry, MeshLineMaterial, raycast } from 'meshline'
 
@@ -18,182 +16,126 @@ declare module '@react-three/fiber' {
   }
 }
 
-export const Blob = ({ route = '/', ...props }) => {
-  const router = useRouter()
-  const [hovered, hover] = useState(false)
-  useCursor(hovered)
-  return (
-    <mesh
-      onClick={ () => router.push(route) }
-      onPointerOver={ () => hover(true) }
-      onPointerOut={ () => hover(false) }
-      { ...props }>
-      <sphereGeometry args={ [1, 64, 64] } />
-      <MeshDistortMaterial roughness={ 0 } color={ hovered ? 'hotpink' : '#1fb2f5' } />
-    </mesh>
-  )
+interface LineProps {
+  points: THREE.Vector3[]
+  color: THREE.ColorRepresentation
+  lineWidth: number
+  position: THREE.Vector3
 }
 
-export const Logo = ({ route = '/blob', ...props }) => {
-  const mesh: Ref<THREE.Group> | undefined = useRef(null)
-  const router = useRouter()
+export function FragmentLine(props: LineProps) {
+  const firstPoint = useRef(props.points[0])
+  const lastPoint = useRef(props.points[props.points.length - 1])
+  const circleRadius = useRef(4)
 
-  const [hovered, hover] = useState(false)
-  const points = useMemo(() => new THREE.EllipseCurve(0, 0, 3, 1.15, 0, 2 * Math.PI, false, 0).getPoints(100), [])
-
-  useCursor(hovered)
-  useFrame((state, delta) => {
-    const t = state.clock.getElapsedTime()
-    if (!mesh.current) return
-    mesh.current.rotation.y = Math.sin(t) * (Math.PI / 8)
-    mesh.current.rotation.x = Math.cos(t) * (Math.PI / 8)
-    mesh.current.rotation.z -= delta / 4
+  const material = new THREE.MeshBasicMaterial({
+    color: props.color,
   })
 
   return (
-    <group ref={ mesh } { ...props }>
-      <Line worldUnits points={ points } color='#1fb2f5' lineWidth={ 0.15 } />
-      <Line worldUnits points={ points } color='#1fb2f5' lineWidth={ 0.15 } rotation={ [0, 0, 1] } />
-      <Line worldUnits points={ points } color='#1fb2f5' lineWidth={ 0.15 } rotation={ [0, 0, -1] } />
-      <mesh
-        onClick={ () => router.push(route) }
-        onPointerOver={ () => hover(true) }
-        onPointerOut={ () => hover(false) }
-      >
-        <sphereGeometry args={ [0.55, 64, 64] } />
-        <meshPhysicalMaterial roughness={ 0 } color={ hovered ? 'hotpink' : '#1fb2f5' } />
+    <group position={props.position}>
+      <mesh raycast={raycast}>
+        <meshLineGeometry
+          points={props.points.flatMap((vector) => [vector.x, vector.y, vector.z])}
+        />
+        <meshLineMaterial lineWidth={1 / 12} color={props.color as THREE.ColorRepresentation} />
+      </mesh>
+      <mesh position={firstPoint.current}>
+        <Circle args={[circleRadius.current, 32]} material={material} />
+      </mesh>
+      <mesh position={lastPoint.current}>
+        <Circle args={[circleRadius.current, 32]} material={material} />
       </mesh>
     </group>
   )
 }
 
-export function Duck(props: any) {
-  const { scene } = useGLTF('/duck.glb')
-
-  useFrame((state, delta) => (scene.rotation.y += delta))
-
-  return <primitive object={ scene } { ...props } />
-}
-
-export function Dog(props: any) {
-  const { scene } = useGLTF('/dog.glb')
-
-  useFrame((state, delta) => (scene.rotation.y += delta))
-
-  return <primitive object={ scene } { ...props } />
-}
-
-interface LineProps {
-  points: THREE.Vector3[];
-  color: THREE.ColorRepresentation;
-  lineWidth: number;
-  position: THREE.Vector3;
-}
-
-export function FragmentLine(props: LineProps) {
-
-  return (
-    // <Line
-    //   points={ props.points }
-    //   color={ props.color as THREE.ColorRepresentation }
-    //   lineWidth={ props.lineWidth }
-    //   position={ props.position }
-    // />
-    <mesh position={props.position} raycast={raycast}>
-      <meshLineGeometry points={props.points.flatMap(vector => [vector.x, vector.y, vector.z])} />
-      <meshLineMaterial lineWidth={1/12} color={props.color as THREE.ColorRepresentation} />
-    </mesh>
-  )
-}
-
 interface CircleProps {
-  radius: number;
-  color: THREE.ColorRepresentation;
-  xCorrection: number;
-  segments: number;
-  pointsList: NotePositionTime[];
-  isAnimating?: boolean;
-  loop?: boolean;
-  onComplete?: () => void;
+  radius: number
+  color: THREE.ColorRepresentation
+  xCorrection: number
+  segments: number
+  pointsList: NotePositionTime[]
+  isAnimating?: boolean
+  loop?: boolean
+  onComplete?: () => void
 }
 
 export function FragmentCircle(props: CircleProps) {
-  const circle = useRef<THREE.Mesh>(null);
-  const segmentIndex = useRef(0);
-  const animationTimer = useRef(0);
-  const segmentTimer = useRef(0);
-  const currentSegment = useRef<NotePositionTime | undefined>(undefined);
+  const circle = useRef<THREE.Mesh>(null)
+  const segmentIndex = useRef(0)
+  const animationTimer = useRef(0)
+  const segmentTimer = useRef(0)
+  const currentSegment = useRef<NotePositionTime | undefined>(undefined)
 
   const material = new THREE.MeshBasicMaterial({
     color: props.color,
-  });
+  })
 
-  const totalAnimationDuration = props.pointsList.reduce((acc, curr) => acc + curr.time, 0);
+  const totalAnimationDuration = props.pointsList.reduce((acc, curr) => acc + curr.time, 0)
 
   const isAnimating = (): boolean => {
     if (!props.isAnimating) {
-      [animationTimer.current, segmentTimer.current, segmentIndex.current] = [0, 0, 0];
-      return false;
+      ;[animationTimer.current, segmentTimer.current, segmentIndex.current] = [0, 0, 0]
+      return false
     }
-    return true;
-  };
+    return true
+  }
 
   const updateTimers = (delta: number): void => {
-    animationTimer.current += delta * 1000;
-    segmentTimer.current += delta * 1000;
-  };
+    animationTimer.current += delta * 1000
+    segmentTimer.current += delta * 1000
+  }
 
   const isAnimationComplete = (): boolean => {
     if (animationTimer.current >= totalAnimationDuration) {
       if (props.loop && props.loop === true) {
-        animationTimer.current = 0;
-        segmentTimer.current = 0;
-        segmentIndex.current = 0;
+        animationTimer.current = 0
+        segmentTimer.current = 0
+        segmentIndex.current = 0
       } else {
-        props.onComplete?.();
-        return true;
+        props.onComplete?.()
+        return true
       }
     }
-    return false;
-  };
+    return false
+  }
 
   const updatePosition = (): void => {
-    currentSegment.current = props.pointsList[segmentIndex.current];
-    if (!currentSegment.current) return;
+    currentSegment.current = props.pointsList[segmentIndex.current]
+    if (!currentSegment.current) return
 
-    const startPoint = currentSegment.current.position[0] as { x: number; y: number };
-    const endPoint = currentSegment.current.position[1] as { x: number; y: number };
+    const startPoint = currentSegment.current.position[0] as { x: number; y: number }
+    const endPoint = currentSegment.current.position[1] as { x: number; y: number }
 
-    const noteLineLength = endPoint.x - startPoint.x;
-    const normalizedTime = segmentTimer.current / currentSegment.current.time;
+    const noteLineLength = endPoint.x - startPoint.x
+    const normalizedTime = segmentTimer.current / currentSegment.current.time
 
-    const newX = startPoint.x + (normalizedTime * noteLineLength);
-    const NewY = startPoint.y;
+    const newX = startPoint.x + normalizedTime * noteLineLength
+    const NewY = startPoint.y
 
-    circle.current?.position.set(newX + props.xCorrection, NewY, 0);
-  };
+    circle.current?.position.set(newX + props.xCorrection, NewY, 0)
+  }
 
   const updateSegmentIndex = (): void => {
-    if (!currentSegment.current) return;
+    if (!currentSegment.current) return
     if (segmentTimer.current > currentSegment.current.time) {
-      segmentTimer.current = 0;
-      segmentIndex.current = (segmentIndex.current + 1) % props.pointsList.length;
+      segmentTimer.current = 0
+      segmentIndex.current = (segmentIndex.current + 1) % props.pointsList.length
     }
-  };
+  }
 
   useFrame((_, delta) => {
-    if (!isAnimating()) return;
+    if (!isAnimating()) return
     if (isAnimationComplete()) return
-    updateTimers(delta);
-    updatePosition();
-    updateSegmentIndex();
-  });
+    updateTimers(delta)
+    updatePosition()
+    updateSegmentIndex()
+  })
 
   return (
-    <mesh position={ circle.current?.position } ref={ circle }>
-      { props.isAnimating &&
-        <Circle args={ [props.radius, props.segments] } material={ material } />
-      }
+    <mesh position={circle.current?.position} ref={circle}>
+      {props.isAnimating && <Circle args={[props.radius, props.segments]} material={material} />}
     </mesh>
-  );
+  )
 }
