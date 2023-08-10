133cae3b5c3b39333fd503e69f482d739da8ef5c
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-08-10 10:45:27 +0200

    fix: editing screen sizes

--- src/components/manage/manageBaseModal.tsx
+++ src/components/manage/manageBaseModal.tsx
@@ -6,7 +6,7 @@ const ManageBaseModal: React.FC<{ title: string; children?: React.ReactNode }> =
     <>
       <div className=" fixed inset-0 z-50 flex items-center justify-center">
         <div className="relative mx-auto my-6 w-1/2 px-4">
-          <div className="relative flex max-h-[50vh] w-full  flex-col overflow-y-auto rounded-lg bg-white p-4 shadow-lg dark:bg-black">
+          <div className="relative flex max-h-[90dvh] w-full  flex-col overflow-y-auto rounded-lg bg-white p-4 shadow-lg dark:bg-black">
             <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
               <h3 className="text-3xl text-center font-semibold">{title}</h3>
             </div>
@@ -14,7 +14,7 @@ const ManageBaseModal: React.FC<{ title: string; children?: React.ReactNode }> =
           </div>
         </div>
       </div>
-      <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+      <div className="fixed inset-0 z-40 bg-slate-800 opacity-80"></div>
     </>
   )
 }
