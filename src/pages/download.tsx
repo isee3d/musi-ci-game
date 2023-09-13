4ff88bf357d774da806ee5687b8a07c89edf5c9b
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-09-13 15:36:59 +0200

    fix excel download

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -4,6 +4,7 @@ import { api } from '~/utils/api'
 import { Parser } from 'json2csv'
 import { Button } from '~/components/ui/button'
 import { useRequireAuth } from '~/hooks/useRequireAuth'
+import { Workbook } from 'exceljs'
 
 function flattenObject(obj: any, prefix = ''): { [key: string]: any } {
   return Object.keys(obj).reduce<{ [key: string]: any }>((acc, k) => {
@@ -22,21 +23,59 @@ const DownloadPage: NextPage = () => {
 
   const downloadQuery = api.download.getAll.useQuery()
 
-  const downloadCSV = async () => {
-  const flattenedData = (downloadQuery.data?.levelResults ?? []).map((result) =>
-    flattenObject(result),
-  )
+  const downloadExcel = async () => {
+    const data = downloadQuery.data
 
-    const opts = { quote: '', delimiter: ';' }
-    const json2csvParser = new Parser(opts)
-    const csv = json2csvParser.parse(flattenedData)
+    if (!data) return
 
-    const blob = new Blob([csv], { type: 'text/csv' })
+    const workbook = new Workbook()
+
+     for (const [sheetName, sheetData] of Object.entries(data)) {
+       const worksheet = workbook.addWorksheet(sheetName)
+
+       // Assuming the data is an array of objects, where each object is a row
+       // and the keys are the column headers
+       if (sheetData.length > 0) {
+         const headers = Object.keys(sheetData[0])
+         worksheet.addRow(headers)
+
+         for (const row of sheetData) {
+           worksheet.addRow(Object.values(row))
+         }
+       }
+     }
+
+    // Generate Excel and trigger download
+    const buffer = await workbook.xlsx.writeBuffer()
+    const blob = new Blob([buffer], {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    })
     const link = document.createElement('a')
     link.href = URL.createObjectURL(blob)
-    link.download = 'data.csv'
+    link.download = 'data.xlsx'
     link.click()
   }
+
+  const downloadJSON = async () => {
+    const data = downloadQuery.data
+
+    if (!data) return
+
+    // Stringify the data to convert it to JSON format
+    const jsonData = JSON.stringify(data, null, 2) // The "2" here formats the JSON with 2-space indentation
+
+    // Create a blob from the JSON string
+    const blob = new Blob([jsonData], {
+      type: 'application/json',
+    })
+
+    // Create a link element, set its href to the blob, and trigger a click to start the download
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(blob)
+    link.download = 'data.json'
+    link.click()
+  }
+
 
   return (
     <>
@@ -50,8 +89,11 @@ const DownloadPage: NextPage = () => {
         <div className="container mx-auto flex flex-col items-center justify-center space-y-8">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">Download CSV</h1>
           <h3 className="text-xl font-extrabold">TEST Download</h3>
-          <Button onClick={downloadCSV} size={'lg'}>
-            <h3>Download nu</h3>
+          <Button onClick={downloadExcel} size={'lg'}>
+            <h3>Download naar csv</h3>
+          </Button>
+          <Button onClick={downloadJSON} size={'lg'}>
+            <h3>Download naar JSON</h3>
           </Button>
         </div>
       </section>
--- src/server/api/routers/download.ts
+++ src/server/api/routers/download.ts
@@ -1,3 +1,4 @@
+import { SceneFragment } from './../../../../prisma/generated/zod/index';
 import { z } from 'zod'
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from '~/server/api/trpc'
@@ -14,22 +15,265 @@ export const downloadRouter = createTRPCRouter({
       },
     })
 
-    const levelResults = await ctx.prisma.levelResult.findMany({
+    const levels = await ctx.prisma.level.findMany({
       select: {
-        user: {
+        id: true,
+        name: true,
+        description: true,
+        color: true,
+        subLevels: {
           select: {
-            participantId: true,
+            id: true,
           },
         },
-        startTime: true,
-        endTime: true,
-        score: true,
+        levelResult: {
+          select: {
+            id: true,
+          },
+        },
       },
     })
 
+    const sublevels = await ctx.prisma.subLevel.findMany({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        color: true,
+
+        levels: {
+          select: {
+            id: true,
+          },
+        },
+        fragments: {
+          select: {
+            id: true,
+          },
+        },
+        gameModes: {
+          select: {
+            id: true,
+          },
+        },
+        levelResult: {
+          select: {
+            id: true,
+          },
+        },
+        questions: {
+          select: {
+            id: true,
+          },
+        },
+        fragmentGroups: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+
+    const fragmentGroups = await ctx.prisma.fragmentGroup.findMany({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        fragments: {
+          select: {
+            id: true,
+          },
+        },
+        subLevels: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+
+    const fragments = await ctx.prisma.fragment.findMany({
+      select: {
+        id: true,
+        name: true,
+        useAlways: true,
+        description: true,
+        isActive: true,
+
+        level: {
+          select: {
+            id: true,
+          },
+        },
+        chosenScene: {
+          select: {
+            id: true,
+          },
+        },
+        playedScene: {
+          select: {
+            id: true,
+          },
+        },
+        relistenfragment: {
+          select: {
+            id: true,
+          },
+        },
+        sceneFragment: {
+          select: {
+            id: true,
+          },
+        },
+        fragmentgroup: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+
+    const notes = await ctx.prisma.note.findMany({
+      select: {
+        id_Fragment: true,
+        id: true,
+        name: true,
+        time: true,
+        duration: true,
+        speed: true,
+      },
+    })
+
+    const gameModes = await ctx.prisma.gameMode.findMany({
+      select: {
+        id: true,
+        name: true,
+        amountOfScenes: true,
+        one: true,
+        two: true,
+        three: true,
+        go: true,
+
+        levels: {
+          select: {
+            id: true,
+          },
+        },
+        levelResult: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+
+    const levelResults = await ctx.prisma.levelResult.findMany({
+      select: {
+        id: true,
+        id_User: true,
+        id_level: true,
+        id_subLevel: true,
+        id_gameMode: true,
+        startTime: true,
+        endTime: true,
+        score: true,
+        Scenes: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+
+      const scenes = await ctx.prisma.scene.findMany({
+        select: {
+          id: true,
+          id_chosenFragment: true,
+          id_levelResult: true,
+          relistenFragments: true,
+          id_playedFragment: true,
+          chosenFragmentLatency: true,
+          answeredCorrectly: true,
+          sceneFragments: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      })
+
+      const sceneFragments = await ctx.prisma.sceneFragment.findMany({
+        select: {
+          id: true,
+          id_fragment: true,
+          id_Scene: true,
+          fragmentIndex: true,
+          groundTone: true,
+          octave: true,
+        },
+      })
+
+      const relistenFragments = await ctx.prisma.relistenFragment.findMany({
+        select: {
+          id: true,
+          id_scene: true,
+          id_fragment: true,
+          relistenCount: true,
+        },
+      })
+
+      const appSettings = await ctx.prisma.appSettings.findMany({
+        select: {
+          id: true,
+          fragmentDotColor: true,
+          fragmentDotLineColor: true,
+        },
+      })
+
+      const questionAnswers = await ctx.prisma.questionAnswer.findMany({
+        select: {
+          id: true,
+          id_User: true,
+          question: true,
+          answer: true,
+          answeredDate: true,
+        },
+      })
+
+      const questions = await ctx.prisma.question.findMany({
+        select: {
+          id: true,
+          id_subLevel: true,
+          question: true,
+        },
+      })
+
+      const activities = await ctx.prisma.activity.findMany({
+        select: {
+          id: true,
+          id_User: true,
+          activity: true,
+          activity_Date: true,
+        },
+      })
+
+
     return {
-        users,
-        levelResults,
+      users,
+      levels,
+      sublevels,
+      fragmentGroups,
+      fragments,
+      notes,
+      gameModes,
+      levelResults,
+      scenes,
+      sceneFragments,
+      relistenFragments,
+      appSettings,
+      questionAnswers,
+      questions,
+      activities,
     }
   }),
 })
