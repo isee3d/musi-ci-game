34c3d000380e67d8a354cd4ef61bc21b39b5be99
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-09-20 16:14:20 +0200

    fix: excel download error

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -6,6 +6,7 @@ import { Button } from '~/components/ui/button'
 import { useRequireAuth } from '~/hooks/useRequireAuth'
 import { Workbook } from 'exceljs'
 import { useRequireResearcherRole } from '~/hooks/useRequireAdminRole'
+import { useEffect, useState } from 'react'
 
 function flattenObject(obj: any, prefix = ''): { [key: string]: any } {
   return Object.keys(obj).reduce<{ [key: string]: any }>((acc, k) => {
@@ -23,54 +24,109 @@ const DownloadPage: NextPage = () => {
   useRequireAuth()
   useRequireResearcherRole()
 
-  const usersQuery = api.download.getAllUsers.useQuery(undefined, {enabled: true})
-  const levelsQuery = api.download.getAllLevels.useQuery(undefined, {enabled: !!usersQuery.data})
-  const sublevelsQuery = api.download.getAllSublevels.useQuery(undefined, {enabled: !!levelsQuery.data})
-  const fragmentGroupsQuery = api.download.getAllFragmentGroups.useQuery(undefined, {enabled: !!sublevelsQuery.data})
-  const fragmentsQuery = api.download.getAllFragments.useQuery(undefined, {enabled: !!fragmentGroupsQuery.data})
-  const notesQuery = api.download.getAllNotes.useQuery(undefined, {enabled: !!fragmentsQuery.data})
-  const gameModesQuery = api.download.getAllGameModes.useQuery(undefined, {enabled: !!notesQuery.data})
-  const levelResultsQuery = api.download.getAllLevelResults.useQuery(undefined, {enabled: !!gameModesQuery.data})
-  const scenesQuery = api.download.getAllScenes.useQuery(undefined, {enabled: !!levelResultsQuery.data})
-  const sceneFragmentsQuery = api.download.getAllSceneFragments.useQuery(undefined, {enabled: !!scenesQuery.data})
-  const relistenFragmentsQuery = api.download.getAllRelistenFragments.useQuery(undefined, {enabled: !!sceneFragmentsQuery.data})
-  const questionAnswersQuery = api.download.getAllQuestionAnswers.useQuery(undefined, {enabled: !!relistenFragmentsQuery.data})
-  const activitiesQuery = api.download.getAllActivities.useQuery(undefined, {enabled: !!questionAnswersQuery.data})
+  const [queriesData, setQueriesData] = useState({})
+  const [shouldDownload, setShouldDownload] = useState(false)
+
+  const usersQuery = api.download.getAllUsers.useQuery(undefined, {
+    enabled: shouldDownload === true,
+  })
+  const levelsQuery = api.download.getAllLevels.useQuery(undefined, { enabled: !!usersQuery.data })
+  const sublevelsQuery = api.download.getAllSublevels.useQuery(undefined, {
+    enabled: !!levelsQuery.data,
+  })
+  const fragmentGroupsQuery = api.download.getAllFragmentGroups.useQuery(undefined, {
+    enabled: !!sublevelsQuery.data,
+  })
+  const fragmentsQuery = api.download.getAllFragments.useQuery(undefined, {
+    enabled: !!fragmentGroupsQuery.data,
+  })
+  const notesQuery = api.download.getAllNotes.useQuery(undefined, {
+    enabled: !!fragmentsQuery.data,
+  })
+  const gameModesQuery = api.download.getAllGameModes.useQuery(undefined, {
+    enabled: !!notesQuery.data,
+  })
+  const levelResultsQuery = api.download.getAllLevelResults.useQuery(undefined, {
+    enabled: !!gameModesQuery.data,
+  })
+  const scenesQuery = api.download.getAllScenes.useQuery(undefined, {
+    enabled: !!levelResultsQuery.data,
+  })
+  const sceneFragmentsQuery = api.download.getAllSceneFragments.useQuery(undefined, {
+    enabled: !!scenesQuery.data,
+  })
+  const relistenFragmentsQuery = api.download.getAllRelistenFragments.useQuery(undefined, {
+    enabled: !!sceneFragmentsQuery.data,
+  })
+  const questionAnswersQuery = api.download.getAllQuestionAnswers.useQuery(undefined, {
+    enabled: !!relistenFragmentsQuery.data,
+  })
+  const activitiesQuery = api.download.getAllActivities.useQuery(undefined, {
+    enabled: !!questionAnswersQuery.data,
+  })
+
+  useEffect(() => {
+    setQueriesData({
+      users: usersQuery.data,
+      levels: levelsQuery.data,
+      sublevels: sublevelsQuery.data,
+      fragmentGroups: fragmentGroupsQuery.data,
+      fragments: fragmentsQuery.data,
+      notes: notesQuery.data,
+      gameModes: gameModesQuery.data,
+      levelResults: levelResultsQuery.data,
+      scenes: scenesQuery.data,
+      sceneFragments: sceneFragmentsQuery.data,
+      relistenFragments: relistenFragmentsQuery.data,
+      questionAnswers: questionAnswersQuery.data,
+      activities: activitiesQuery.data,
+    })
+  }, [
+    usersQuery.data,
+    levelsQuery.data,
+    sublevelsQuery.data,
+    fragmentGroupsQuery.data,
+    fragmentsQuery.data,
+    notesQuery.data,
+    gameModesQuery.data,
+    levelResultsQuery.data,
+    scenesQuery.data,
+    sceneFragmentsQuery.data,
+    relistenFragmentsQuery.data,
+    questionAnswersQuery.data,
+    activitiesQuery.data,
+  ])
 
   const downloadExcel = async () => {
-    const data = usersQuery.data
-
-    if (!data) return
-
     const workbook = new Workbook()
 
-    for (const [sheetName, sheetData] of Object.entries(data)) {
-      const worksheet = workbook.addWorksheet(sheetName)
+    for (const [sheetName, sheetData] of Object.entries(queriesData)) {
+      if (!sheetData || sheetData.length === 0) continue // Skip if no data
 
-      // Assuming the data is an array of objects, where each object is a row
-      // and the keys are the column headers
-      if (sheetData.length > 0) {
-        const headers = Object.keys(sheetData[0])
-        worksheet.addRow(headers)
+      const worksheet = workbook.addWorksheet(
+        sheetName.charAt(0).toUpperCase() + sheetName.slice(1),
+      ) // Capitalize sheet name for better aesthetics
 
-        for (const row of sheetData) {
-          worksheet.addRow(Object.values(row))
-        }
+      const headers = Object.keys(sheetData[0])
+      worksheet.addRow(headers)
 
-        // Adjust column width
-        worksheet.columns.forEach((column) => {
-          let maxColumnLength = 0
-
-          column.eachCell({ includeEmpty: true }, (cell) => {
-            const columnLength = cell.text.length
-            if (columnLength > maxColumnLength) {
-              maxColumnLength = columnLength
-            }
-          })
-
-          column.width = maxColumnLength + 2 // Add some padding
-        })
+      for (const row of sheetData) {
+        worksheet.addRow(Object.values(row))
       }
+
+      // Adjust column width
+      worksheet.columns.forEach((column) => {
+        let maxColumnLength = 0
+
+        column.eachCell({ includeEmpty: true }, (cell) => {
+          const columnLength = cell.text.length
+          if (columnLength > maxColumnLength) {
+            maxColumnLength = columnLength
+          }
+        })
+
+        column.width = maxColumnLength + 2 // Add some padding
+      })
     }
 
     // Generate Excel and trigger download
@@ -84,26 +140,29 @@ const DownloadPage: NextPage = () => {
     link.click()
   }
 
-  const downloadJSON = async () => {
-    const data = usersQuery.data
-
-    if (!data) return
-
-    // Stringify the data to convert it to JSON format
-    const jsonData = JSON.stringify(data, null, 2) // The "2" here formats the JSON with 2-space indentation
-
-    // Create a blob from the JSON string
-    const blob = new Blob([jsonData], {
-      type: 'application/json',
-    })
-
-    // Create a link element, set its href to the blob, and trigger a click to start the download
-    const link = document.createElement('a')
-    link.href = URL.createObjectURL(blob)
-    link.download = 'data.json'
-    link.click()
+  function startDownload() {
+    setShouldDownload(true)
   }
 
+  // const downloadJSON = async () => {
+  //   const data = usersQuery.data
+
+  //   if (!data) return
+
+  //   // Stringify the data to convert it to JSON format
+  //   const jsonData = JSON.stringify(data, null, 2) // The "2" here formats the JSON with 2-space indentation
+
+  //   // Create a blob from the JSON string
+  //   const blob = new Blob([jsonData], {
+  //     type: 'application/json',
+  //   })
+
+  //   // Create a link element, set its href to the blob, and trigger a click to start the download
+  //   const link = document.createElement('a')
+  //   link.href = URL.createObjectURL(blob)
+  //   link.download = 'data.json'
+  //   link.click()
+  // }
 
   return (
     <>
@@ -116,13 +175,21 @@ const DownloadPage: NextPage = () => {
       <section className=" relative flex grow flex-col items-center justify-center bg-cover bg-no-repeat">
         <div className="container mx-auto flex flex-col items-center justify-center space-y-8">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">Download CSV</h1>
-          <h3 className="text-xl font-extrabold">TEST Download</h3>
-          <Button onClick={downloadExcel} size={'lg'}>
-            <h3>Download naar csv</h3>
+          <Button onClick={startDownload} size={'lg'}>
+            <h3>Klik hier om de download te starten, dit kan even duren</h3>
           </Button>
-          <Button onClick={downloadJSON} size={'lg'}>
+
+          {shouldDownload && !activitiesQuery.data && <h2>De download is bezig...</h2>}
+
+          {activitiesQuery.data && (
+            <Button onClick={downloadExcel} size={'lg'}>
+              <h3>Download naar csv</h3>
+            </Button>
+          )}
+
+          {/* <Button onClick={downloadJSON} size={'lg'}>
             <h3>Download naar JSON</h3>
-          </Button>
+          </Button> */}
         </div>
       </section>
     </>
