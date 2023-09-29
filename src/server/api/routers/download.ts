0f98b12860013e82f1d9ad77a71c468ca1c6b02f
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2023-09-29 12:27:31 +0200

    fix: excel layout

--- src/server/api/routers/download.ts
+++ src/server/api/routers/download.ts
@@ -22,10 +22,26 @@ export const downloadRouter = createTRPCRouter({
   }),
   getAllLevels: publicProcedure.query(async ({ ctx }) => {
     const levels = await ctx.prisma.level.findMany({
-      include: {
-        subLevels: true,
-        levelResult: true,
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        color: true,
+        subLevels: {
+          select: {
+            id: true,
+          },
+        },
+        levelResult: {
+          select: {
+            id: true,
+          },
+        },
       },
+      // include: {
+      //   subLevels: true,
+      //   levelResult: true,
+      // },
     })
 
     return levels
@@ -33,14 +49,51 @@ export const downloadRouter = createTRPCRouter({
 
   getAllSublevels: publicProcedure.query(async ({ ctx }) => {
     const sublevels = await ctx.prisma.subLevel.findMany({
-      include: {
-        levels: true,
-        fragments: true,
-        gameModes: true,
-        levelResult: true,
-        questions: true,
-        fragmentGroups: true,
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        color: true,
+        levels: {
+          select: {
+            id: true,
+          },
+        },
+        fragments: {
+          select: {
+            id: true,
+          },
+        },
+        gameModes: {
+          select: {
+            id: true,
+          },
+        },
+        levelResult: {
+          select: {
+            id: true,
+          },
+        },
+        questions: {
+          select: {
+            id: true,
+          },
+        },
+        fragmentGroups: {
+          select: {
+            id: true,
+          },
+        },
       },
+
+      // include: {
+      //   levels: true,
+      //   fragments: true,
+      //   gameModes: true,
+      //   levelResult: true,
+      //   questions: true,
+      //   fragmentGroups: true,
+      // },
     })
 
     return sublevels
@@ -48,10 +101,26 @@ export const downloadRouter = createTRPCRouter({
 
   getAllFragmentGroups: publicProcedure.query(async ({ ctx }) => {
     const fragmentGroups = await ctx.prisma.fragmentGroup.findMany({
-      include: {
-        fragments: true,
-        subLevels: true,
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        fragments: {
+          select: {
+            id: true,
+          },
+        },
+        subLevels: {
+          select: {
+            id: true,
+          },
+        },
       },
+
+      // include: {
+      //   fragments: true,
+      //   subLevels: true,
+      // },
     })
 
     return fragmentGroups
@@ -59,15 +128,53 @@ export const downloadRouter = createTRPCRouter({
 
   getAllFragments: publicProcedure.query(async ({ ctx }) => {
     const fragments = await ctx.prisma.fragment.findMany({
-      include: {
-        notes: true,
-        level: true,
-        chosenScene: true,
-        playedScene: true,
-        relistenfragment: true,
-        sceneFragment: true,
-        fragmentgroup: true,
+      select: {
+        id: true,
+        name: true,
+        useAlways: true,
+        description: true,
+        isActive: true,
+        level: {
+          select: {
+            id: true,
+          },
+        },
+        chosenScene: {
+          select: {
+            id: true,
+          },
+        },
+        playedScene: {
+          select: {
+            id: true,
+          },
+        },
+        relistenfragment: {
+          select: {
+            id: true,
+          },
+        },
+        sceneFragment: {
+          select: {
+            id: true,
+          },
+        },
+        fragmentgroup: {
+          select: {
+            id: true,
+          },
+        },
       },
+
+      // include: {
+      //   notes: true,
+      //   level: true,
+      //   chosenScene: true,
+      //   playedScene: true,
+      //   relistenfragment: true,
+      //   sceneFragment: true,
+      //   fragmentgroup: true,
+      // },
     })
 
     return fragments
@@ -75,9 +182,18 @@ export const downloadRouter = createTRPCRouter({
 
   getAllNotes: publicProcedure.query(async ({ ctx }) => {
     const notes = await ctx.prisma.note.findMany({
-      include: {
-        fragment: true,
+      select: {
+        id_Fragment: true,
+        id: true,
+        name: true,
+        time: true,
+        duration: true,
+        speed: true,
       },
+
+      // include: {
+      //   fragment: true,
+      // },
     })
 
     return notes
@@ -85,10 +201,30 @@ export const downloadRouter = createTRPCRouter({
 
   getAllGameModes: publicProcedure.query(async ({ ctx }) => {
     const gameModes = await ctx.prisma.gameMode.findMany({
-      include: {
-        levels: true,
-        levelResult: true,
+      select: {
+        id: true,
+        name: true,
+        amountOfScenes: true,
+        one: true,
+        two: true,
+        three: true,
+        go: true,
+        levels: {
+          select: {
+            id: true,
+          },
+        },
+        levelResult: {
+          select: {
+            id: true,
+          },
+        },
       },
+
+      // include: {
+      //   levels: true,
+      //   levelResult: true,
+      // },
     })
 
     return gameModes
@@ -96,13 +232,29 @@ export const downloadRouter = createTRPCRouter({
 
   getAllLevelResults: publicProcedure.query(async ({ ctx }) => {
     const levelResults = await ctx.prisma.levelResult.findMany({
-      include: {
-        user: true,
-        Level: true,
-        subLevel: true,
-        gameMode: true,
-        Scenes: true,
+      select: {
+        id: true,
+        id_User: true,
+        id_level: true,
+        id_subLevel: true,
+        id_gameMode: true,
+        startTime: true,
+        endTime: true,
+        score: true,
+        Scenes: {
+          select: {
+            id: true,
+          },
+        },
       },
+
+      // include: {
+      //   user: true,
+      //   Level: true,
+      //   subLevel: true,
+      //   gameMode: true,
+      //   Scenes: true,
+      // },
     })
 
     return levelResults
@@ -110,13 +262,28 @@ export const downloadRouter = createTRPCRouter({
 
   getAllScenes: publicProcedure.query(async ({ ctx }) => {
     const scenes = await ctx.prisma.scene.findMany({
-      include: {
-        chosenFragment: true,
-        sceneFragments: true,
-        levelResult: true,
+      select: {
+        id: true,
+        id_chosenFragment: true,
+        id_levelResult: true,
         relistenFragments: true,
-        playedFragment: true,
+        id_playedFragment: true,
+        chosenFragmentLatency: true,
+        answeredCorrectly: true,
+        sceneFragments: {
+          select: {
+            id: true,
+          },
+        },
       },
+
+      // include: {
+      //   chosenFragment: true,
+      //   sceneFragments: true,
+      //   levelResult: true,
+      //   relistenFragments: true,
+      //   playedFragment: true,
+      // },
     })
 
     return scenes
@@ -124,10 +291,19 @@ export const downloadRouter = createTRPCRouter({
 
   getAllSceneFragments: publicProcedure.query(async ({ ctx }) => {
     const sceneFragments = await ctx.prisma.sceneFragment.findMany({
-      include: {
-        fragment: true,
-        scene: true,
+      select: {
+        id: true,
+        id_fragment: true,
+        id_Scene: true,
+        fragmentIndex: true,
+        groundTone: true,
+        octave: true,
       },
+
+      // include: {
+      //   fragment: true,
+      //   scene: true,
+      // },
     })
 
     return sceneFragments
@@ -135,10 +311,16 @@ export const downloadRouter = createTRPCRouter({
 
   getAllRelistenFragments: publicProcedure.query(async ({ ctx }) => {
     const relistenFragments = await ctx.prisma.relistenFragment.findMany({
-      include: {
-        scene: true,
-        fragment: true,
+      select: {
+        id: true,
+        id_scene: true,
+        id_fragment: true,
+        relistenCount: true,
       },
+      // include: {
+      //   scene: true,
+      //   fragment: true,
+      // },
     })
 
     return relistenFragments
@@ -146,420 +328,47 @@ export const downloadRouter = createTRPCRouter({
 
   getAllQuestionAnswers: publicProcedure.query(async ({ ctx }) => {
     const questionAnswer = await ctx.prisma.questionAnswer.findMany({
-      include: {
-        user: true,
+      select: {
+        id: true,
+        id_User: true,
+        question: true,
+        answer: true,
+        answeredDate: true,
       },
+      // include: {
+      //   user: true,
+      // },
     })
 
     return questionAnswer
   }),
 
+  getAllQuestions: publicProcedure.query(async ({ ctx }) => {
+    const questions = await ctx.prisma.question.findMany({
+      select: {
+        id: true,
+        id_subLevel: true,
+        question: true,
+      },
+    })
+
+    return questions
+  }),
+
   getAllActivities: publicProcedure.query(async ({ ctx }) => {
     const activities = await ctx.prisma.activity.findMany({
-      include: {
-        user: true,
+      select: {
+        id: true,
+        id_User: true,
+        activity: true,
+        activity_Date: true,
       },
+
+      // include: {
+      //   user: true,
+      // },
     })
 
     return activities
   }),
-
-
-  // getAll: protectedProcedure.query(async ({ ctx }) => {
-  //   const users = await ctx.prisma.user.findMany({
-  //     select: {
-  //       id: true,
-  //       participantId: true,
-  //       role: true,
-  //       isAllowedToPlay: true,
-  //       createdAt: true,
-  //       levelResults: true,
-  //       questionAnswers: true,
-  //       activities: true,
-  //     },
-  //   })
-
-  //   const levels = await ctx.prisma.level.findMany({
-  //     include: {
-  //       subLevels: true,
-  //       levelResult: true,
-  //     },
-  //   })
-
-  //   const sublevels = await ctx.prisma.subLevel.findMany({
-  //     include: {
-  //       levels: true,
-  //       fragments: true,
-  //       gameModes: true,
-  //       levelResult: true,
-  //       questions: true,
-  //       fragmentGroups: true,
-  //     },
-  //   })
-
-  //   const fragmentGroups = await ctx.prisma.fragmentGroup.findMany({
-  //     include: {
-  //       fragments: true,
-  //       subLevels: true,
-  //     },
-  //   })
-
-  //   const fragments = await ctx.prisma.fragment.findMany({
-  //     include: {
-  //       notes: true,
-  //       level: true,
-  //       chosenScene: true,
-  //       playedScene: true,
-  //       relistenfragment: true,
-  //       sceneFragment: true,
-  //       fragmentgroup: true,
-  //     },
-  //   })
-
-  //   const notes = await ctx.prisma.note.findMany({
-  //     include: {
-  //       fragment: true,
-  //     },
-  //   })
-
-  //   const gameModes = await ctx.prisma.gameMode.findMany({
-  //     include: {
-  //       levels: true,
-  //       levelResult: true,
-  //     },
-  //   })
-
-  //   const levelResults = await ctx.prisma.levelResult.findMany({
-  //     include: {
-  //       user: true,
-  //       Level: true,
-  //       subLevel: true,
-  //       gameMode: true,
-  //       Scenes: true,
-  //     },
-  //   })
-
-  //   const scenes = await ctx.prisma.scene.findMany({
-  //     include: {
-  //       chosenFragment: true,
-  //       sceneFragments: true,
-  //       levelResult: true,
-  //       relistenFragments: true,
-  //       playedFragment: true,
-  //     },
-  //   })
-
-  //   const sceneFragments = await ctx.prisma.sceneFragment.findMany({
-  //     include: {
-  //       fragment: true,
-  //       scene: true,
-  //     },
-  //   })
-
-  //   const relistenFragments = await ctx.prisma.relistenFragment.findMany({
-  //     include: {
-  //       scene: true,
-  //       fragment: true,
-  //     },
-  //   })
-
-  //   const questionAnswer = await ctx.prisma.questionAnswer.findMany({
-  //     include: {
-  //       user: true,
-  //     },
-  //   })
-
-  //   const activities = await ctx.prisma.activity.findMany({
-  //     include: {
-  //       user: true,
-  //     },
-  //   })
-
-
-
-
-
-
-  //   // const users = await ctx.prisma.user.findMany({
-  //   //   select: {
-  //   //     id: true,
-  //   //     participantId: true,
-  //   //     role: true,
-  //   //     isAllowedToPlay: true,
-  //   //     createdAt: true,
-  //   //   },
-  //   // })
-
-  //   // const levels = await ctx.prisma.level.findMany({
-  //   //   select: {
-  //   //     id: true,
-  //   //     name: true,
-  //   //     description: true,
-  //   //     color: true,
-  //   //     subLevels: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     levelResult: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //   },
-  //   // })
-
-  //   // const sublevels = await ctx.prisma.subLevel.findMany({
-  //   //   select: {
-  //   //     id: true,
-  //   //     name: true,
-  //   //     description: true,
-  //   //     color: true,
-
-  //   //     levels: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     fragments: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     gameModes: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     levelResult: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     questions: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     fragmentGroups: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //   },
-  //   // })
-
-  //   // const fragmentGroups = await ctx.prisma.fragmentGroup.findMany({
-  //   //   select: {
-  //   //     id: true,
-  //   //     name: true,
-  //   //     description: true,
-  //   //     fragments: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     subLevels: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //   },
-  //   // })
-
-  //   // const fragments = await ctx.prisma.fragment.findMany({
-  //   //   select: {
-  //   //     id: true,
-  //   //     name: true,
-  //   //     useAlways: true,
-  //   //     description: true,
-  //   //     isActive: true,
-
-  //   //     level: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     chosenScene: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     playedScene: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     relistenfragment: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     sceneFragment: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     fragmentgroup: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //   },
-  //   // })
-
-  //   // const notes = await ctx.prisma.note.findMany({
-  //   //   select: {
-  //   //     id_Fragment: true,
-  //   //     id: true,
-  //   //     name: true,
-  //   //     time: true,
-  //   //     duration: true,
-  //   //     speed: true,
-  //   //   },
-  //   // })
-
-  //   // const gameModes = await ctx.prisma.gameMode.findMany({
-  //   //   select: {
-  //   //     id: true,
-  //   //     name: true,
-  //   //     amountOfScenes: true,
-  //   //     one: true,
-  //   //     two: true,
-  //   //     three: true,
-  //   //     go: true,
-
-  //   //     levels: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //     levelResult: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //   },
-  //   // })
-
-  //   // const levelResults = await ctx.prisma.levelResult.findMany({
-  //   //   select: {
-  //   //     id: true,
-  //   //     id_User: true,
-  //   //     id_level: true,
-  //   //     id_subLevel: true,
-  //   //     id_gameMode: true,
-  //   //     startTime: true,
-  //   //     endTime: true,
-  //   //     score: true,
-  //   //     Scenes: {
-  //   //       select: {
-  //   //         id: true,
-  //   //       },
-  //   //     },
-  //   //   },
-  //   // })
-
-  //   //   const scenes = await ctx.prisma.scene.findMany({
-  //   //     select: {
-  //   //       id: true,
-  //   //       id_chosenFragment: true,
-  //   //       id_levelResult: true,
-  //   //       relistenFragments: true,
-  //   //       id_playedFragment: true,
-  //   //       chosenFragmentLatency: true,
-  //   //       answeredCorrectly: true,
-  //   //       sceneFragments: {
-  //   //         select: {
-  //   //           id: true,
-  //   //         },
-  //   //       },
-  //   //     },
-  //   //   })
-
-  //   //   const sceneFragments = await ctx.prisma.sceneFragment.findMany({
-  //   //     select: {
-  //   //       id: true,
-  //   //       id_fragment: true,
-  //   //       id_Scene: true,
-  //   //       fragmentIndex: true,
-  //   //       groundTone: true,
-  //   //       octave: true,
-  //   //     },
-  //   //   })
-
-  //   //   const relistenFragments = await ctx.prisma.relistenFragment.findMany({
-  //   //     select: {
-  //   //       id: true,
-  //   //       id_scene: true,
-  //   //       id_fragment: true,
-  //   //       relistenCount: true,
-  //   //     },
-  //   //   })
-
-  //   //   const appSettings = await ctx.prisma.appSettings.findMany({
-  //   //     select: {
-  //   //       id: true,
-  //   //       fragmentDotColor: true,
-  //   //       fragmentDotLineColor: true,
-  //   //     },
-  //   //   })
-
-  //   //   const questionAnswers = await ctx.prisma.questionAnswer.findMany({
-  //   //     select: {
-  //   //       id: true,
-  //   //       id_User: true,
-  //   //       question: true,
-  //   //       answer: true,
-  //   //       answeredDate: true,
-  //   //     },
-  //   //   })
-
-  //   //   const questions = await ctx.prisma.question.findMany({
-  //   //     select: {
-  //   //       id: true,
-  //   //       id_subLevel: true,
-  //   //       question: true,
-  //   //     },
-  //   //   })
-
-  //   //   const activities = await ctx.prisma.activity.findMany({
-  //   //     select: {
-  //   //       id: true,
-  //   //       id_User: true,
-  //   //       activity: true,
-  //   //       activity_Date: true,
-  //   //     },
-  //   //   })
-
-  //   return {
-  //     users,
-  //     levels,
-  //     sublevels,
-  //     fragmentGroups,
-  //     fragments,
-  //     notes,
-  //     gameModes,
-  //     levelResults,
-  //     scenes,
-  //     sceneFragments,
-  //     relistenFragments,
-  //     questionAnswer,
-  //     activities,
-  //     // levels,
-  //     // sublevels,
-  //     // fragmentGroups,
-  //     // fragments,
-  //     // notes,
-  //     // gameModes,
-  //     // levelResults,
-  //     // scenes,
-  //     // sceneFragments,
-  //     // relistenFragments,
-  //     // appSettings,
-  //     // questionAnswers,
-  //     // questions,
-  //     // activities,
-  //   }
-  // }),
 })
