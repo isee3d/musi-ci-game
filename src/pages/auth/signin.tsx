a3a797cb8a31ebea2b3400b1463c6e3e9a7805b3
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2024-02-01 10:08:16 +0100

    fix: styling of login page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { signIn } from 'next-auth/react'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
@@ -15,6 +16,7 @@ import {
   FormMessage,
 } from '~/components/ui/form'
 import { Input } from '~/components/ui/input'
+import { routePaths } from '~/config/routing'
 
 const Signin = () => {
   const router = useRouter()
@@ -32,65 +34,69 @@ const Signin = () => {
     })
     if (signinResponse?.error) {
       toast.error(`Het wachtwoord of Deelnemer nummer is incorrect`, { duration: 2500 })
-    } else if(signinResponse?.url) {
+    } else if (signinResponse?.url) {
       router.push(signinResponse.url)
       form.reset()
     }
   }
 
   return (
-    <>
+    <div className='w-full flex justify-center'>
       <Form {...form}>
         <form
           autoComplete="off"
           onSubmit={form.handleSubmit(onSubmit)}
-          className="flex  flex-col justify-center gap-y-5 p-40 text-center align-middle"
+          className="flex h-screen w-1/2 flex-col justify-center gap-8 p-24"
         >
-          <h2 className="text-5xl font-bold">Login met deelnemernummer en wachtwoord</h2>
-          <FormField
-            control={form.control}
-            name="participantId"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Deelnemer nummer</FormLabel>
-                <FormControl>
-                  <Input autoComplete="new-password" placeholder="Deelnemer nummer" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Wachtwoord</FormLabel>
-                <FormControl>
-                  <Input
-                    autoComplete="new-password"
-                    type="password"
-                    placeholder="Vul wachtwoord in"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Login</Button>
-          <Button
-            onClick={() => {
-              router.back()
-            }}
-            type="button"
-            className="mx-3"
-          >
-            Terug
-          </Button>
+          <h2 className="text-balance text-center py-4 text-4xl font-bold">
+            Musi-CI Melody Game Oefen met toonhoogtes en korte melodietjes
+          </h2>
+          <div className="flex flex-col gap-2">
+            <FormField
+              control={form.control}
+              name="participantId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Deelnemer nummer</FormLabel>
+                  <FormControl>
+                    <Input autoComplete="new-password" placeholder="Deelnemer nummer" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Wachtwoord</FormLabel>
+                  <FormControl>
+                    <Input
+                      autoComplete="new-password"
+                      type="password"
+                      placeholder="Vul wachtwoord in"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex w-full justify-start gap-4">
+            <Button className="w-1/2" type="submit">
+              Inloggen
+            </Button>
+            <Button className="w-1/2" asChild>
+              <Link href={routePaths.home}>
+              Annuleren
+              </Link>
+            </Button>
+          </div>
         </form>
       </Form>
-    </>
+    </div>
   )
 }
 
