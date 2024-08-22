628e1e469aed7652548b786e292aac5e8c94ff1e
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2024-08-22 16:45:47 +0200

    fix: small issue with video for compatibility

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,6 +1,6 @@
 export function Video() {
   return (
-    <video className="h-full w-full" width="1920" height="1080" controls autoPlay loop>
+    <video className="h-full w-full" width="1920" height="1080" controls playsInline loop>
       <source src="/media/sample-video.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
