8a16c051f2180f4f23454d66a9b211d5a980523f
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2024-09-02 12:33:51 +0200

    fix: open file in tab instead of download

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -49,31 +49,31 @@ async function runTestSound() {
 
 async function downloadPdf(): Promise<void> {
   try {
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-    ) {
+    // if (
+    //   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    // ) {
       // For mobile devices, just try to open in a new tab
       window.open('/media/sample.pdf', '_blank')
-      return
-    }
+      // return
+    // }
 
-    const response = await fetch('/media/sample.pdf')
-    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
-    const blob = await response.blob()
-    const url = URL.createObjectURL(blob)
+  //   const response = await fetch('/media/sample.pdf')
+  //   if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`)
+  //   const blob = await response.blob()
+  //   const url = URL.createObjectURL(blob)
 
-    const link = document.createElement('a')
-    link.href = url
-    link.target = '_blank'
-    link.download = 'uitleg-musi-ci-2024.pdf'
+  //   const link = document.createElement('a')
+  //   link.href = url
+  //   link.target = '_blank'
+  //   link.download = 'uitleg-musi-ci-2024.pdf'
 
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-    URL.revokeObjectURL(url)
-  } catch (error) {
-    console.error('Error downloading the PDF:', error)
-  }
+  //   document.body.appendChild(link)
+  //   link.click()
+  //   document.body.removeChild(link)
+  //   URL.revokeObjectURL(url)
+  // } catch (error) {
+  //   console.error('Error downloading the PDF:', error)
+  // }
 }
 
 export const navItemsPlayer: PlayerNavItem[] = [
