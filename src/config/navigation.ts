823956f65c1ae145e8b827f12f76d0b9c372120f
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2024-09-02 11:45:24 +0200

    fix: download file on mobile

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -47,13 +47,24 @@ async function runTestSound() {
   await testSound()
 }
 
-function downloadPdf() {
-    const link = document.createElement('a');
-    link.href = '/media/sample.pdf';
-    link.download = 'uitleg-musi-ci-2024.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+async function downloadPdf() {
+   try {
+     const response = await fetch('/media/sample.pdf')
+     const blob = await response.blob()
+     const url = URL.createObjectURL(blob)
+
+     const link = document.createElement('a')
+     link.href = url
+     link.download = 'uitleg-musi-ci-2024.pdf'
+     document.body.appendChild(link)
+
+     link.click()
+
+     document.body.removeChild(link)
+     URL.revokeObjectURL(url)
+   } catch (error) {
+     console.error('Error downloading the PDF:', error)
+   }
 }
 
 export const navItemsPlayer: PlayerNavItem[] = [
