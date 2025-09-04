e33b1369b652083c5ddbdf60fa088fcdb0cd9ca6
vanboxsel-engineering <accounts@vanboxsel.nl>
2025-09-04 14:47:05 +0200

    create generic function for transposing tests

--- src/utils/testUtils.ts
+++ src/utils/testUtils.ts
@@ -1,7 +1,6 @@
-import {
-  FragmentWithNotes
-} from '~/components/fragmentPlayer/audio/fragmentWithNotes'
+import { FragmentWithNotes } from '~/components/fragmentPlayer/audio/fragmentWithNotes'
 import { log } from '~/components/gameModes/testMode/test'
+import { TestScene } from '~/components/gameModes/testMode/testJsonData'
 import { useLuisterenStore } from '~/stores/gameModes/luisterenStore'
 import { useAudioServiceStore } from '~/stores/useAudioServiceStore'
 
@@ -19,8 +18,49 @@ function getFragmentByName({
   return fragments.find((fragment) => fragment.name === fragmentName)
 }
 
+export function transposeTestN(testIndex: number, fragments: FragmentWithNotes[]) {
+  function getFragment(fragmentName?: string) {
+    if (!fragmentName) return
+    return getFragmentByName({ fragmentName, fragments })
+  }
+
+  const key = `Test${testIndex}Array` as string
+  const { [key]: testArray } = useLuisterenStore.getState() as unknown as Record<
+    string,
+    TestScene[]
+  >
+  if (!testArray) throw new Error(`${key} not found`)
+  const { removeItemFromTestLevel } = useLuisterenStore.getState()
+  const { transposeFragments } = useAudioServiceStore.getState()
+
+  const nextTestItem = Math.floor(Math.random() * testArray.length)
+  const scene = testArray[nextTestItem]
+
+  if (!scene) throw new Error(`Scene not found in test_${testIndex}, ${nextTestItem}`)
+  log.push(scene.scene)
+
+  const allFragments = [
+    getFragment(scene.fragment_1),
+    getFragment(scene.fragment_2),
+    getFragment(scene.fragment_3),
+  ].filter(Boolean) as FragmentWithNotes[]
+
+  const newActiveFragment = getFragmentByName({
+    fragmentName: scene.afspelen,
+    fragments: fragments,
+  })
+
+  if (!newActiveFragment) throw new Error(`no newActiveFragment`)
+
+  const transposedFragments = transposeFragments(allFragments, scene.octaaf, scene.grondtoon)
+
+  removeItemFromTestLevel(testIndex, nextTestItem)
+
+  return { transposedFragments, newActiveFragment }
+}
+
 export function transposeTestOne(fragments: FragmentWithNotes[]) {
-  const { TestOneArray, removeItemFromTestOneArray } = useLuisterenStore.getState()
+  const { Test1Array: TestOneArray, removeItemFromTestOneArray } = useLuisterenStore.getState()
   if (!TestOneArray) throw new Error('TestOneArray not found')
   const nextTestItem = Math.floor(Math.random() * TestOneArray.length)
   const scene = TestOneArray[nextTestItem]
@@ -44,7 +84,7 @@ export function transposeTestOne(fragments: FragmentWithNotes[]) {
 }
 
 export function transposeTestTwo(fragments: FragmentWithNotes[]) {
-  const { TestTwoArray, removeItemFromTestTwoArray } = useLuisterenStore.getState()
+  const { Test2Array: TestTwoArray, removeItemFromTestTwoArray } = useLuisterenStore.getState()
   if (!TestTwoArray) throw new Error('TestOneArray not found')
   const nextTestItem = Math.floor(Math.random() * TestTwoArray.length)
   const scene = TestTwoArray[nextTestItem]
